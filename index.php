406d5fa2ecd136827302dd5c49b6f35bc075f0fe
Mohammad Nasim <nasimsweb@gmail.com>
2014-11-10 19:28:33 +0500

    IE Bug  version 1 to 12 version
    Structure : $("YOUR TAGs").css({"Attribute":"value","Attribute":"value"});
    In Use :     $("body").css({"background-color":"red","color":"#cde"});

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
+<html>
+<head>
+<title>Your Title</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<script language="JavaScript" type="text/javascript" src="css/jquery.js"></script>
+<script>
+function GetIEVersion() {
+  var sAgent = window.navigator.userAgent;
+  var Idx = sAgent.indexOf("MSIE");
+ 
+  // If IE, return version number.
+  if (Idx > 0)
+    return parseInt(sAgent.substring(Idx+ 5, sAgent.indexOf(".", Idx)));
+  // If IE 11 then look for Updated user agent string.
+  else if (!!navigator.userAgent.match(/Trident\/7\./))
+    return 11;
+  else
+    return 0; //It is not IE
+}
+function a(){
+if (GetIEVersion() > 0)
+$("BODY").CSS({"BACKGROUND-COLOR":"RED"});
+else
+   alert("This is not IE.");
+}
+</script>
+</head>
+<body onload='a()'>
+<h1>IE BUGs</h1>
+</body>
+</html>
